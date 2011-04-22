1dcb4d0fed00acbf0387442ebc9f7a4158c9b7be
syncrou <drew@bomhofintegrated.com>
2011-04-22 08:20:23 -0700

    Removed call to pluralize table names. Going to handle it ar_biosql.rb so the bioruby tables don't interfere with the railsian table names set for the other non bioruby classes

--- lib/bio/io/biosql/biosql.rb
+++ lib/bio/io/biosql/biosql.rb
@@ -9,7 +9,7 @@ module Bio
       #NOTE: Using postgresql, not setting sequence name, system will discover the name by default.
       #NOTE: this class will not establish the connection automatically
       self.abstract_class = true
-      self.pluralize_table_names = false
+      #self.pluralize_table_names = false
       #prepend table name to the usual id, avoid to specify primary id for every table
       self.primary_key_prefix_type = :table_name_with_underscore
       #biosql_configurations=YAML::load(ERB.new(IO.read(File.join(File.dirname(__FILE__),'./config', 'database.yml'))).result)
