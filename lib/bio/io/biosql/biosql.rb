143d5181e5d15db92fd278cb32aa13cd313d96b6
syncrou <drew@bomhofintegrated.com>
2011-04-22 08:28:27 -0700

    Also pulled out the need to have table_name_id for primary keys (for every model in the rails app)

--- lib/bio/io/biosql/biosql.rb
+++ lib/bio/io/biosql/biosql.rb
@@ -11,7 +11,7 @@ module Bio
       self.abstract_class = true
       #self.pluralize_table_names = false
       #prepend table name to the usual id, avoid to specify primary id for every table
-      self.primary_key_prefix_type = :table_name_with_underscore
+      #self.primary_key_prefix_type = :table_name_with_underscore
       #biosql_configurations=YAML::load(ERB.new(IO.read(File.join(File.dirname(__FILE__),'./config', 'database.yml'))).result)
       #self.configurations=biosql_configurations
       #self.establish_connection "development"
